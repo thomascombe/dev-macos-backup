a46274487a305d2619e1562abc3f5e17a5c18c8e
Thomas COMBE <tcombe@webqam.fr>
2021-09-29 19:49:16 +0200

    remove zip backup

--- scripts/projects-zip.sh
+++ scripts/projects-zip.sh
@@ -2,9 +2,6 @@ source ./includes/variables.sh
 
 backup() {
   mkdir -p "$backupFolder/projects-zip"
-
-  rm "$backupFolder/projects-zip/backup.tar.gz.bak"
-  mv "$backupFolder/projects-zip/backup.tar.gz" "$backupFolder/projects-zip/backup.tar.gz.bak"
   tar -czf "$backupFolder/projects-zip/backup.tar.gz" --exclude="node_modules" --exclude="vendor" --exclude="var/cache" --exclude="storage/framework" "$projectsFolder"
 }
 
