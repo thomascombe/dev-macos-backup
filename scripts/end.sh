9ae363781df7f66a321589bdc0bb147b903f6ad8
Thomas COMBE <tcombe@webqam.fr>
2021-09-30 21:30:37 +0200

    Add slack notification

--- scripts/end.sh
+++ scripts/end.sh
@@ -1,3 +1,7 @@
 source ./includes/variables.sh
 
 commit
+
+if [[ $SLACK_ENABLED = "true" ]]; then
+    curl -X POST --data-urlencode "payload={\"channel\": \"$SLACK_CHANNEL\", \"username\": \"$MACHINE_NAME Backup\", \"text\": \"Backup ended!\", \"icon_emoji\": \":heavy_check_mark:\"}" $SLACK_WEBHOOK
+fi
