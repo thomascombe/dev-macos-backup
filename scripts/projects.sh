c75aed1d03a4f6d81979e924e38dd7d6e7c9ae75
Thomas COMBE <tcombe@webqam.fr>
2021-11-15 17:22:11 +0100

    delete old file on project rsync

--- scripts/projects.sh
+++ scripts/projects.sh
@@ -5,7 +5,7 @@ source ./includes/variables.sh
 backup() {
   mkdir -p "$backupFolder/projects"
 
-  rsync --compress -rltvI --ignore-existing "$projectsFolder" --exclude="node_modules" --exclude="vendor" --exclude="var/cache" --exclude="storage/framework" "$backupFolder/projects"
+  rsync --compress -rltvI --ignore-existing --delete "$projectsFolder" --exclude="node_modules" --exclude="vendor" --exclude="var/cache" --exclude="storage/framework" "$backupFolder/projects"
 }
 
 restore() {
